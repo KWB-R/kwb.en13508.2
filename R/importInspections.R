75ba01759ed52f81c9de7d61619a99f1fd917052
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-07-01 10:10:34 +0200

    Add new version of getInspectionHeaderInfo()
    using kwb.utils::extractSubstring()

--- R/importInspections.R
+++ R/importInspections.R
@@ -54,6 +54,37 @@ getInspectionHeaderInfo <- function(eu_lines)
   stats::setNames(header_rows, unique_headers)
 }
 
+getInspectionHeaderInfo2 <- function(eu_lines)
+{
+  # Get list of matching sub expressions
+  matches <- kwb.utils::extractSubstring("^#B(\\d\\d)=(.*)$", eu_lines, c(
+    number = 1L, fields = 2L
+  ))
+  
+  matches$row <- seq_along(eu_lines)
+  
+  # Indices of header lines
+  header_indices <- which(!kwb.utils::isNaOrEmpty(matches$fields))
+  
+  # Number of header (#B01 = 1, #B02 = 2)
+  header_numbers <- as.integer(matches$number[header_indices])
+  
+  # Only the header (right of equal sign)
+  header_lines <- matches$fields[header_indices]
+  
+  unique_headers <- unique(header_lines)
+  
+  # For each different type of header, determine the line numbers in which it
+  # occurs
+  header_rows <- lapply(unique_headers, function(header) {
+    indices <- which(header_lines == header)
+    stopifnot(kwb.utils::allAreEqual(header_numbers[indices]))
+    list(line = header_numbers[indices[1L]], rows = header_indices[indices])
+  })
+  
+  stats::setNames(header_rows, unique_headers)
+}
+
 # extractInspectionBlocks ------------------------------------------------------
 
 extractInspectionBlocks <- function(
