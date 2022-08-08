84a6e2c475e33f513988d6c776a2211169680fe4
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2022-08-08 02:07:08 +0200

    Use nzchar(), add commented microbenchmark code

--- R/getInspectionHeaderInfo_v2.R
+++ R/getInspectionHeaderInfo_v2.R
@@ -1,9 +1,18 @@
+# eu_lines <- readLines(kwb.en13508.2:::getExampleFile())
+# 
+# microbenchmark::microbenchmark(
+#   x = kwb.en13508.2:::getInspectionHeaderInfo_v1(eu_lines),
+#   y = kwb.en13508.2:::getInspectionHeaderInfo_v2(eu_lines),
+#   times = 1000,
+#   check = "identical"
+# )
+
 # getInspectionHeaderInfo_v2 ---------------------------------------------------
 getInspectionHeaderInfo_v2 <- function(eu_lines)
 {
   pattern <- "^#B(\\d\\d)=(.*)$"
   
-  # Get list of matching sub expressions
+  # Get data frame with one row per line and matching sub expressions
   matches <- kwb.utils::extractSubstring(
     pattern = pattern, 
     x = eu_lines, 
@@ -13,7 +22,7 @@ getInspectionHeaderInfo_v2 <- function(eu_lines)
   matches$row <- seq_along(eu_lines)
   
   # Indices of header lines
-  header_indices <- which(!kwb.utils::isNaOrEmpty(matches$fields))
+  header_indices <- which(nzchar(matches$fields))
   
   # Number of header (#B01 = 1, #B02 = 2)
   header_numbers <- as.integer(matches$number[header_indices])
