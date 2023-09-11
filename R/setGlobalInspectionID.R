9f0689f8e9de75004b0e887d5e33b059610e6dee
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-09-11 19:20:43 +0200

    Create time "on the fly" if time column missing

--- R/setGlobalInspectionID.R
+++ R/setGlobalInspectionID.R
@@ -7,10 +7,18 @@
 #'   \code{inspections}, \code{observations}
 #' @param project name of project to which the data are related, such as:
 #'   "Lausanne"
+#' @param defaultTime default time string to use if column InspTime is not
+#'   available. Default: "12:00". A random number will be generated for the 
+#'   seconds, just to increase the chance that setting the time is enough to
+#'   generate a unique key.
 #' @return list with the same elements as in \code{inspection.data} but with
 #'   columns \code{inspid} being added to the data frames "inspections" and
 #'   "observations"
-setGlobalInspectionID <- function(inspection.data, project = NULL)
+setGlobalInspectionID <- function(
+    inspection.data, 
+    project = NULL, 
+    default.time = "12:00"
+)
 {
   if (is.null(project)) {
     stop(
@@ -19,22 +27,53 @@ setGlobalInspectionID <- function(inspection.data, project = NULL)
     )
   }
   
-  header.info <- kwb.utils::selectElements(inspection.data, "header.info")
-  inspections <- kwb.utils::selectElements(inspection.data, "inspections")
-  observations <- kwb.utils::selectElements(inspection.data, "observations")
+  fetch <- kwb.utils::createAccessor(inspection.data)
+  
+  # Just a shortcut
+  removeEmpty <- function(df) kwb.utils::removeEmptyColumns(df, dbg = FALSE)
 
-  inspections <- kwb.utils::removeEmptyColumns(inspections)
+  inspections <- removeEmpty(fetch("inspections"))
+  observations <- removeEmpty(fetch("observations"))
+  
   inspections[["project"]] <- project
+  
+  # The following function requires the column "InspTime". If this column does
+  # not exist, create it with a default value
+  if (is.null(inspections[["InspTime"]])) {
+    
+    message(
+      "There is no column 'InspTime' (inspection time). ", 
+      "I will create this column\n", 
+      "and set it to '", default.time, "' (plus random seconds) ", 
+      "for each inspection.\n", 
+      "You may change this time value by setting the argument 'default.time'."
+    )
+
+    # We have to fix the random number generator otherwise the times are not
+    # reproducible!
+    set.seed(123L)
+    
+    # Generate a random number for the seconds
+    inspections[["InspTime"]] <- sprintf(
+      "%s:%02d", 
+      default.time, 
+      sample(0:59, size = nrow(inspections), replace = TRUE)
+    )
+  }
+  
+  # Create the inspection IDs and store them in column "inspid"
   inspections[["inspid"]] <- createInspectionId(inspections)
 
-  observations <- kwb.utils::removeEmptyColumns(observations)
   i <- kwb.utils::selectColumns(observations, "inspno")
   observations[["inspid"]] <- kwb.utils::selectColumns(inspections, "inspid")[i]
   observations <- kwb.utils::removeColumns(observations, "inspno")
 
+  # Just a shortcut
+  inspidFirst <- function(df) kwb.utils::moveColumnsToFront(df, "inspid")
+
   list(
-    header.info = header.info,
-    inspections = kwb.utils::moveColumnsToFront(inspections, "inspid"),
-    observations = kwb.utils::moveColumnsToFront(observations, "inspid")
+    header.info = fetch("header.info"),
+    inspections = inspidFirst(inspections),
+    observations = inspidFirst(observations)
   )
 }
