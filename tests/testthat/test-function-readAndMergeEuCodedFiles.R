1c87aab21081606ed3d2d807a6c6fba2417a3936
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-11-28 21:39:20 +0100

    Test for camel/snake case column names

--- tests/testthat/test-function-readAndMergeEuCodedFiles.R
+++ tests/testthat/test-function-readAndMergeEuCodedFiles.R
@@ -1,10 +1,34 @@
+#library(testthat)
 test_that("readAndMergeEuCodedFiles() works", {
 
   f <- kwb.en13508.2:::readAndMergeEuCodedFiles
   
   expect_error(f())
   
-  file <- getExampleFile()
+  file <- kwb.en13508.2:::getExampleFile()
+  files <- c(file, file)
+    
+  result_camel <- f(files, meaningful.names = TRUE)
+  result_snake <- f(files, meaningful.names = TRUE, snake.case = TRUE)
   
-  result <- f(c(file, file))
+  elements_ok <- function(x) expect_identical(names(x), c(
+    "header.info", "inspections", "observations"
+  ))
+  
+  elements_ok(result_camel)
+  elements_ok(result_snake)
+  
+  expect_true(all(
+    c("MainCode", "Char1", "Char2") %in% names(result_camel$observations)
+  ))
+  
+  expect_true(all(
+    c("main_code", "char_1", "char_2") %in% names(result_snake$observations)
+  ))
+
+  expect_true(!anyNA(result_camel$observations$Char1))
+  expect_true(!anyNA(result_camel$observations$Char2))
+  
+  expect_true(!anyNA(result_snake$observations$char_1))
+  expect_true(!anyNA(result_snake$observations$char_2))
 })
