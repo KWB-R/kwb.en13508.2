d4930920a38ded34dd6b0f59c4571a8eda0d7994
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2023-11-30 09:18:17 +0100

    Add test for extractObservationBlocks()

--- tests/testthat/test-function-extractObservationBlocks.R
+++ tests/testthat/test-function-extractObservationBlocks.R
@@ -5,4 +5,31 @@ test_that("extractObservationBlocks() works", {
   
   expect_error(f())
 
+  euLines <- c(
+    "#C=A,B,C", #1
+    "1,2,3",    #2
+    "#Z",       #3
+    "#C=A,B",   #4
+    "11,22",    #5
+    "#Z"        #6
+  )
+  
+  headerInfo <- data.frame(
+    row = as.integer(c(1, 3, 4, 6)),
+    type      = c("C", "Z", "C", "Z"),
+    uniqueKey = c("1", "", "2", "")
+  )
+  
+  expect_identical(f(euLines, headerInfo, uniqueKey = "no-such-key"), list())
+  
+  expect_identical(
+    f(euLines, headerInfo, uniqueKey = "1"),
+    list("1,2,3")
+  )
+  
+  expect_identical(
+    f(euLines, headerInfo, uniqueKey = "2"),
+    list("11,22")
+  )
+  
 })
