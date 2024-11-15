7755619596a8b9584afeea07b28595c25be0ddfe
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-11-15 21:53:55 +0100

    Add argument "makeUnique"

--- R/createHashFromColumns.R
+++ R/createHashFromColumns.R
@@ -1,28 +1,41 @@
 # createHashFromColumns --------------------------------------------------------
-createHashFromColumns <- function(data, columns, nchars = 8L, silent = FALSE)
+createHashFromColumns <- function(
+    data, columns, nchars = 8L, silent = FALSE, makeUnique = FALSE
+)
 {
   duplicates <- kwb.utils::findPartialDuplicates(data, columns)
   
   if (!is.null(duplicates)) {
-    
     if (!silent) {
       message(
         "Cannot create unique hashes due to duplicates in the key columns (",
         kwb.utils::stringList(columns), 
-        ")! Returning -1L. Check attribute 'duplicates'."
+        ")! "
       )
+      if (makeUnique) {
+        message("I will make the hashes unique.")
+      } else {
+        message("Returning -1L. Check attribute 'duplicates'.")
+      }
     }
-    
-    return(structure(-1L, duplicates = duplicates))
+    if (!makeUnique) {
+      return(structure(-1L, duplicates = duplicates))
+    }
   }
   
   keys <- kwb.utils::pasteColumns(data, columns, "|")
-  
-  stopifnot(!anyDuplicated(keys))
+
+  if (!makeUnique) {
+    stopifnot(!anyDuplicated(keys))
+  }
   
   hashes <- kwb.utils::left(unlist(lapply(keys, digest::digest)), nchars)
   
-  stopifnot(!anyDuplicated(hashes))
+  if (makeUnique) {
+    hashes <- kwb.utils::makeUnique(hashes, warn = FALSE)
+  }
   
+  stopifnot(!anyDuplicated(hashes))
+
   hashes
 }
